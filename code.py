c7b951029e636b6f2cf1f49f8c756b1f61f07812
MrProha <mrprohagames@gmail.com>
2022-09-25 22:36:08 +0300

    in process

--- code.py
+++ code.py
@@ -1,18 +1,22 @@
 class FlatIterator:
     def __init__(self, list_):
         self.list_ = list_
+        self.list_index = -1
+        self.result = []
 
     def __iter__(self):
         self.cursor = self.list_[0]
         return self
 
     def __next__(self):
-        self.cursor = self.list_[self.list_.index(self.cursor)+1]
-
+        self.list_index+=1
+        self.cursor = self.list_[self.list_index]
+        for i in self.cursor:
+            self.result.append(i)
         if self.cursor == self.list_[len(self.list_)-1]:
             raise StopIteration
-        return self.cursor
+        return self.result
 
 nested_list = [['a', 'b', 'c'], ['d', 'e', 'f', 'h', False], [1, 2, None]]
-for item in FlatIterator(nested_list):
-	print(item)
+flat_list = [item for item in FlatIterator(nested_list)][0]
+print(flat_list)

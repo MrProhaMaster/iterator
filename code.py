60ecd6a3baad9a0ecd7389563a4f35dcc0e767e1
MrProha <mrprohagames@gmail.com>
2022-10-04 17:55:19 +0300

    task2 - done

--- code.py
+++ code.py
@@ -19,10 +19,23 @@ class FlatIterator:
         return self.list_[self.list_index][self.in_list_index]
 
 
-
+def gen(nested_list):
+    result_list = []
+    for i in nested_list:
+        if isinstance(i, list):
+            for n in i:
+                result_list.append(n)
+        else:
+            result_list.append(i)
+    for item in result_list:
+        yield item
 
 nested_list = [['a', 'b', 'c'], ['d', 'e', 'f', 'h', False], [1, 2, None]]
-for item in FlatIterator(nested_list):
+
+for item in gen(nested_list):
     print(item)
-flat_list = [item for item in FlatIterator(nested_list)]
-print(flat_list)
+
+#for item in FlatIterator(nested_list):
+#    print(item)
+#flat_list = [item for item in FlatIterator(nested_list)]
+#print(flat_list)
